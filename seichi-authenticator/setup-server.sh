171ae70fb46eaa892b0a4708f0e1a2bb0e6a585f
Kory <korygm33@gmail.com>
2021-12-24 23:18:07 +0900

    feat: add script for installing docker

--- seichi-authenticator/setup-server.sh
+++ seichi-authenticator/setup-server.sh
@@ -8,4 +8,36 @@ set -e
 #   - restart all the services on reboot
 # - reboots the server to reinitialize everything
 
+sudo apt-get -y update && sudo apt-get -y upgrade
+
+# region install misc toolchains
+
+# required for docker (https://docs.docker.com/engine/install/ubuntu/)
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+# endregion
+
+# region install docker (see https://docs.docker.com/engine/install/ubuntu/)
+
+sudo apt-get remove docker docker-engine docker.io containerd runc || true
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+
+echo "======================================"
+echo "done installing docker-ce. docker --version output:"
+docker --version
+echo "======================================"
+
+# endregion
+
+echo "all setup complete. rebooting..."
 sudo reboot
