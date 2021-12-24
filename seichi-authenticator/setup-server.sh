f7fffa19f3f6a4d7ba4393c06dab498cb9ca6a11
Kory <korygm33@gmail.com>
2021-12-24 23:31:45 +0900

    fix: use -y for installing docker

--- seichi-authenticator/setup-server.sh
+++ seichi-authenticator/setup-server.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
+function echo-bar () {
+    echo "======================================"
+}
+
 # This is an idempotent script that
 # - installs all the required toolchains
 # - configures the server to
@@ -29,15 +33,17 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt-get update && sudo apt-get -y install docker-ce docker-ce-cli containerd.io
 
-echo "======================================"
+echo-bar
 echo "done installing docker-ce. docker --version output:"
 docker --version
-echo "======================================"
+echo-bar
 
 # endregion
 
+echo-bar
 echo "all setup complete. rebooting..."
+echo-bar
+
 sudo reboot
