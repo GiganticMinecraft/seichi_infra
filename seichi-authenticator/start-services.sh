b756cc47153e87c931623619785b8ec23e64d535
Kory <korygm33@gmail.com>
2021-12-25 10:25:21 +0900

    feat: サービスを開始させるようにする

--- seichi-authenticator/start-services.sh
+++ seichi-authenticator/start-services.sh
@@ -7,3 +7,19 @@ timedatectl set-timezone Asia/Tokyo
 echo 'reboot' | at 4:00
 
 # endregion
+
+# region start services
+
+if [ ! -f "/root/seichi_open_servers_configs/.keycloak.env" ]; then
+  echo "ERROR: expected an env file at"
+  echo "  /root/seichi_open_servers_configs/.keycloak.env"
+  echo "Exiting the script..."
+  exit 1
+fi
+
+docker compose \
+  -e KEYCLOAK_ENV_FILE_PATH=/root/seichi_open_servers_configs/.keycloak.env
+  -f /root/seichi_open_servers/seichi-authenticator/docker-compose.yml \
+  up
+
+# endregion
