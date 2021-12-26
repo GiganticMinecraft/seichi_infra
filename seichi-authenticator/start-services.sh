0a48646fb619d80e8ff6146c5365cef60a555663
Kory <korygm33@gmail.com>
2021-12-26 15:33:12 +0900

    feat: postgres生やす

--- seichi-authenticator/start-services.sh
+++ seichi-authenticator/start-services.sh
@@ -10,15 +10,23 @@ echo 'reboot' | at 4:00
 
 # region start services
 
-if [ ! -f "/root/seichi_open_servers_configs/.keycloak.env" ]; then
-  echo "ERROR: expected an env file at"
-  echo "  /root/seichi_open_servers_configs/.keycloak.env"
-  echo "Exiting the script..."
-  exit 1
-fi
+env_files=(
+  "/root/seichi_open_servers_configs/.keycloak.env"
+  "/root/seichi_open_servers_configs/.postgres.env"
+)
+
+for env_file in "${env_files[@]}"; do
+  if [ ! -f "$env_file" ]; then
+    echo "ERROR: expected an env file at"
+    echo "  $env_file"
+    echo "Exiting the script..."
+    exit 1
+  fi
+done
 
 docker compose \
   -e KEYCLOAK_ENV_FILE_PATH=/root/seichi_open_servers_configs/.keycloak.env
+  -e POSTGRES_ENV_FILE_PATH=/root/seichi_open_servers_configs/.postgres.env
   -f /root/seichi_open_servers/seichi-authenticator/docker-compose.yml \
   up
 
