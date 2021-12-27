b5f5411d467416badcefc75915db159a2bd0f7f2
Kory <korygm33@gmail.com>
2021-12-28 04:24:42 +0900

    feat: cloudflare_access_identity_provider.github_oauthを追加

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,6 +4,10 @@ terraform {
       source = "cloudflare/cloudflare"
       version = "~> 3.0"
     }
+    github = {
+      source  = "integrations/github"
+      version = "~> 4.0"
+    }
   }
 
   cloud {
@@ -27,12 +31,52 @@ variable "cloudflare_api_key" {
   sensitive   = true
 }
 
-locals {
-  cloudflare_zone_id = "77c10fdfa7c65de4d14903ed8879ebcb"
-  root_domain = "seichi.click"
-}
-
 provider "cloudflare" {
   email   = var.cloudflare_email
   api_key = var.cloudflare_api_key
+}
+
+variable "github_cloudflare_oauth_client_id" {
+  description = "Client ID of Cloudflare as seens as an OAuth App on GitHub"
+  type        = string
+  sensitive   = true
+}
+
+variable "github_cloudflare_oauth_client_secret" {
+  description = "Client secret of Cloudflare as seens as an OAuth App on GitHub"
+  type        = string
+  sensitive   = true
+}
+
+variable "terraform_github_app_id" {
+  description = "Client ID of the GitHub App used for Terraform automation"
+  type        = string
+  sensitive   = true
+}
+
+# Found at
+# https://github.com/organizations/GiganticMinecraft/settings/installations/:installation_id
+variable "terraform_github_app_installation_id" {
+  description = "Client installation ID of the GitHub App used for Terraform automation"
+  type        = string
+  sensitive   = true
+}
+
+variable "terraform_github_app_pem" {
+  description = "Client private key of the GitHub App used for Terraform automation"
+  type        = string
+  sensitive   = true
+}
+
+provider "github" {
+  app_auth {
+    id              = var.terraform_github_app_id
+    installation_id = var.terraform_github_app_installation_id
+    pem_file        = var.terraform_github_app_pem
+  }
+}
+
+locals {
+  cloudflare_zone_id = "77c10fdfa7c65de4d14903ed8879ebcb"
+  root_domain = "seichi.click"
 }
