a0b21cd9e46e18f8741b23c73f8c0076066014a9
Kory <korygm33@gmail.com>
2021-12-28 05:36:18 +0900

    fix: teamの可視性をclosedにする

--- terraform/github_teams.tf
+++ terraform/github_teams.tf
@@ -1,12 +1,16 @@
 resource "github_team" "nginx_test_connection_team" {
   name        = "nginx-test-connection-team"
   description = "テスト用のTeam。このTeamのメンバーのみ public-nginx.test.seichi.click にアクセスできるはず。"
-  privacy     = "secret"
+  # 外部からは見えないっぽいのでこれでOK
+  #  > Visible teams can be viewed and @mentioned by every organization member.
+  #    https://docs.github.com/en/organizations/organizing-members-into-teams/changing-team-visibility
+  # 具体的なメンバーは別リポジトリで管理すれば良い
+  privacy     = "closed"
 }
 
 resource "github_team" "nginx_test_connection_team_nested" {
   name        = "nginx-test-connection-team-nested"
   description = "テスト用のTeam。"
   parent_team_id = github_team.nginx_test_connection_team.id
-  privacy     = "secret"  
+  privacy     = "closed"  
 }
