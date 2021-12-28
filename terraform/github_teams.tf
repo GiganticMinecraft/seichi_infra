a581d1c1182edf1a1598dc0850f33332f5a09611
Kory | Ryosuke Kondo <6561358+kory33@users.noreply.github.com>
2021-12-29 01:44:59 +0900

    feat: GitHub Teamsを定義する

--- terraform/github_teams.tf
+++ terraform/github_teams.tf
@@ -1,16 +1,28 @@
-resource "github_team" "nginx_test_connection_team" {
-  name        = "nginx-test-connection-team"
-  description = "テスト用のTeam。このTeamのメンバーのみ public-nginx.test.seichi.click にアクセスできるはず。"
-  # 外部からは見えないっぽいのでこれでOK
-  #  > Visible teams can be viewed and @mentioned by every organization member.
-  #    https://docs.github.com/en/organizations/organizing-members-into-teams/changing-team-visibility
-  # 具体的なメンバーは別リポジトリで管理すれば良い
+# 外部からは見えないっぽいので privacy = "closed" でOK
+#  > Visible teams can be viewed and @mentioned by every organization member.
+#    https://docs.github.com/en/organizations/organizing-members-into-teams/changing-team-visibility
+# 具体的なメンバーは別リポジトリ (GiganticMinecraft/teams) で管理することとする
+
+resource "github_team" "admin_team" {
+  name        = "Admin team"
+  description = "[Terraform] 運営チーム"
   privacy     = "closed"
 }
 
-resource "github_team" "nginx_test_connection_team_nested" {
-  name        = "nginx-test-connection-team-nested"
-  description = "テスト用のTeam。"
-  parent_team_id = github_team.nginx_test_connection_team.id
-  privacy     = "closed"  
+resource "github_team" "nginx_test_connection_team" {
+  name        = "Infra Collaborator"
+  description = "[Terraform]"
+  privacy     = "closed"
+}
+
+resource "github_team" "nginx_test_connection_team" {
+  name        = "nginx-test-connection-team"
+  description = "[Terraform] テスト用のTeam。このTeamのメンバーのみ public-nginx.test.seichi.click にアクセスできるはず。"
+  privacy     = "closed"
+}
+
+resource "github_team" "nginx_test_connection_team" {
+  name        = "Springer"
+  description = "[Terraform] 整地鯖(春)のデベロッパー"
+  privacy     = "closed"
 }
