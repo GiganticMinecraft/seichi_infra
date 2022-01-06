ecf6aede8c4eaaa2009131bc15223136913a1703
unchama <11990197+unchama@users.noreply.github.com>
2022-01-06 21:39:22 +0900

    lkeクラスタにcloudflareの認証用secretを生やす

--- terraform/lke_cluster_secrets.tf
+++ terraform/lke_cluster_secrets.tf
@@ -26,3 +26,16 @@ resource "kubernetes_secret" "prod_cloudflared_access_service_token" {
   type = "Opaque"
 }
 
+resource "kubernetes_secret" "kube-system_cloudflare_api_key" {
+  metadata {
+    name      = "cloudflare-secret"
+    namespace = "kube-system"
+  }
+
+  data = {
+    CLOUDFLARE_EMAIL   = var.cloudflare_email
+    CLOUDFLARE_API_KEY = var.cloudflare_api_key
+  }
+
+  type = "Opaque"
+}
