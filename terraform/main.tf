8ff93483b2dd751bb3ffe606a470d30164c834d3
Kory | Ryosuke Kondo <6561358+kory33@users.noreply.github.com>
2022-01-06 16:31:08 +0900

    Revert "Access service tokenをLKEのSecretとして入れる"

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,14 +19,6 @@ terraform {
   }
 }
 
-locals {
-  cloudflare_zone_id = "77c10fdfa7c65de4d14903ed8879ebcb"
-  root_domain = "seichi.click"
-  github_org_name = "GiganticMinecraft"
-}
-
-# region cloudflare provider
-
 variable "cloudflare_email" {
   description = "email used for Cloudflare API authentication"
   type        = string
@@ -44,10 +36,6 @@ provider "cloudflare" {
   api_key = var.cloudflare_api_key
 }
 
-# endregion
-
-# region cloudflare-github integration settings
-
 variable "github_cloudflare_oauth_client_id" {
   description = "Client ID of Cloudflare as seens as an OAuth App on GitHub"
   type        = string
@@ -59,10 +47,6 @@ variable "github_cloudflare_oauth_client_secret" {
   type        = string
   sensitive   = true
 }
-
-# endregion
-
-# region terraform-github integration settings
 
 variable "terraform_github_app_id" {
   description = "Client ID of the GitHub App used for Terraform automation"
@@ -93,27 +77,8 @@ provider "github" {
   }
 }
 
-# endregion
-
-# region proxy-layer's k8s access configuration
-
-variable "lke_k8s_kubeconfig" {
-  description   = "LKE cluster's kubeconfig.yaml content"
-  type          = string
-  sensitive     = true
+locals {
+  cloudflare_zone_id = "77c10fdfa7c65de4d14903ed8879ebcb"
+  root_domain = "seichi.click"
+  github_org_name = "GiganticMinecraft"
 }
-
-# LKEの kubeconfig.yaml は、host、userのtokenとcluster CA certificateをそれぞれ
-#  - clusters[?].cluster.server にplaintextで
-#  - users[?].user.token にplaintextで
-#  - clusters[?].cluster.certificate-authority-data にbase64で
-# 保持している。
-# LKE以外のmanaged k8s環境へ乗り換える際には、クラスタから得られるkubeconfigが含む情報が異なる可能性があるので注意。
-
-provider "kubernetes" {
-  host                   = yamldecode(var.lke_k8s_kubeconfig).clusters[0].cluster.server
-  token                  = yamldecode(var.lke_k8s_kubeconfig).users[0].user.token
-  cluster_ca_certificate = base64decode(yamldecode(var.lke_k8s_kubeconfig).clusters[0].cluster.certificate-authority-data)
-}
-
-# endregion
