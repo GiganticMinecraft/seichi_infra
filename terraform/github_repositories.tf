e196f854d65fd8842ca2c1eed8d971e0f761d3ba
Kory <korygm33@gmail.com>
2022-01-15 12:45:52 +0000

    feat: add branch protection rule

---
+++ terraform/github_repositories.tf
@@ -0,0 +1,25 @@
+data "github_repository" "seichi_infra" {
+  full_name = "GiganticMinecraft/seichi_infra"
+}
+
+resource "github_branch_protection" "example" {
+  repository_id = github_repository.seichi_infra.node_id
+
+  pattern          = "main"
+  enforce_admins   = true
+  allows_deletions = false
+
+  required_status_checks {
+    strict   = true
+    contexts = [
+        "Terraform Cloud/GiganticMinecraft/seichi_infra",
+        "shellcheck with reviewdog",
+        "runner/tflint"
+    ]
+  }
+
+  required_pull_request_reviews {
+    dismiss_stale_reviews  = true
+    required_approving_review_count = 1
+  }
+}
