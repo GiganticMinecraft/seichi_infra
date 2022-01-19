b22f099dab209fb33c291ae817d280261d9d1619
Kory | Ryosuke Kondo <6561358+kory33@users.noreply.github.com>
2022-01-19 20:39:48 +0900

    feat: cloudflare_page_rule.reosurce_packのpriorityを指定する

--- terraform/cloudflare_page_rules.tf
+++ terraform/cloudflare_page_rules.tf
@@ -1,19 +1,23 @@
-resource "cloudflare_page_rule" "resource_packs" {
-  zone_id = local.cloudflare_zone_id
-  # TODO ちゃんとしたドメインへ移行
-  target = "s1-map-gigantic.${local.root_domain}/resourcepacks/*"
-
-  actions {
-    browser_check = "off"
-  }
-}
-
 resource "cloudflare_page_rule" "seichi_maps" {
   zone_id = local.cloudflare_zone_id
+  priority = 1
   target = "*-map-gigantic.${local.root_domain}/*"
 
   actions {
     security_level = "under_attack"
+  }
+}
+
+resource "cloudflare_page_rule" "resource_packs" {
+  zone_id = local.cloudflare_zone_id
+  # seichi_mapsルールから除外したいため、priorityを指定しておく
+  priority = 2
+  # TODO ちゃんとしたドメインへ移行
+  target = "s1-map-gigantic.${local.root_domain}/resourcepacks/*"
+
+  actions {
+    security_level = "high"
+    browser_check = "off"
   }
 }
 
