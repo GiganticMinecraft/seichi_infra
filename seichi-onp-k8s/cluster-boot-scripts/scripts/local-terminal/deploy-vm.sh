c57746b433143c93f9fd5df7107ffccf51d02240
Kory <korygm33@gmail.com>
2022-05-24 10:33:01 +0100

    refactor: deploy-vm.shを一個下の階層に入れる

--- seichi-onp-k8s/cluster-boot-scripts/scripts/local-terminal/deploy-vm.sh
+++ seichi-onp-k8s/cluster-boot-scripts/scripts/local-terminal/deploy-vm.sh
@@ -100,7 +100,7 @@ runcmd:
   - su - cloudinit -c "curl -sS https://github.com/kory33.keys >> ~/.ssh/authorized_keys"
   - su - cloudinit -c "chmod 600 ~/.ssh/authorized_keys"
   # run install scripts
-  - su - cloudinit -c "curl -s ${REPOSITORY_RAW_SOURCE_URL}/seichi-onp-k8s/cluster-boot-scripts/scripts/k8s-node-setup.sh > ~/k8s-node-setup.sh"
+  - su - cloudinit -c "curl -s ${REPOSITORY_RAW_SOURCE_URL}/seichi-onp-k8s/cluster-boot-scripts/scripts/nodes/k8s-node-setup.sh > ~/k8s-node-setup.sh"
   - su - cloudinit -c "sudo bash ~/k8s-node-setup.sh ${vmname}"
 EOF
 # ----- #
