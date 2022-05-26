92f403db296f027125e185c66543efbbe2127199
inductor <kela@inductor.me>
2022-05-26 09:57:24 +0900

    Enable Control Plane Metrics

--- seichi-onp-k8s/cluster-boot-up/scripts/nodes/k8s-node-setup.sh
+++ seichi-onp-k8s/cluster-boot-up/scripts/nodes/k8s-node-setup.sh
@@ -290,6 +290,12 @@ apiServer:
   #   https://k8s-api.onp-k8s.admin.local-tunnels.seichi.click:PORT
   # where PORT is the port at which the local tunnel is running.
   - k8s-api.onp-k8s.admin.local-tunnels.seichi.click
+controllerManager:
+  extraArgs:
+    bind-address: "0.0.0.0"
+scheduler:
+  extraArgs:
+    bind-address: "0.0.0.0"
 ---
 apiVersion: kubelet.config.k8s.io/v1beta1
 kind: KubeletConfiguration
