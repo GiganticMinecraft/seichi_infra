afec245d0232676d06daed32f2bfbc966b5c196f
Kory <korygm33@gmail.com>
2022-05-28 10:11:15 +0100

    fix: fix data reference

--- terraform/onp_cluster/proxy/output.tf
+++ terraform/onp_cluster/proxy/output.tf
@@ -1,4 +1,4 @@
 output "cluster_host" {
   value = "https://${local.onp_kubernetes_tunnel_entry_host}:${local.onp_kubernetes_tunnel_entry_port}"
-  depends_on= [ external.proxy_to_onp_k8s_api ]
+  depends_on= [ data.external.proxy_to_onp_k8s_api ]
 }
