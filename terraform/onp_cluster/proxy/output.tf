47df4e82ac4a4054324a37c6c1bd4d6f20a96e39
Kory <korygm33@gmail.com>
2022-05-28 11:13:08 +0100

    Revert "debug: cluster_hostがnull_resourceに依存するように"
    This reverts commit e512f52b8ef6e9704c1b2235f2a7464b6e342a5d.

--- terraform/onp_cluster/proxy/output.tf
+++ terraform/onp_cluster/proxy/output.tf
@@ -1,4 +1,8 @@
 output "cluster_host" {
   value = "https://${local.onp_kubernetes_tunnel_entry_host}:${local.onp_kubernetes_tunnel_entry_port}"
-  depends_on = [ null_resource.proxy_to_onp_k8s_api ]
+  depends_on= [ data.external.proxy_to_onp_k8s_api ]
+}
+
+output "empty_string_dependent_on_null_resource" {
+  value = substr(null_resource.proxy_to_onp_k8s_api.id, 0, 0)
 }
