90e22236070cbf2e965cc6dc3438cd777a93e908
Kory <korygm33@gmail.com>
2022-06-04 13:43:27 +0100

    feat: 新URLの保護ルールを追加

--- terraform/cloudflare_page_rules.tf
+++ terraform/cloudflare_page_rules.tf
@@ -1,7 +1,37 @@
-resource "cloudflare_page_rule" "seichi_maps" {
+resource "cloudflare_page_rule" "seichi_maps_old" {
   zone_id  = local.cloudflare_zone_id
   priority = 1
   target   = "*-map-gigantic.${local.root_domain}/*"
+
+  actions {
+    security_level = "under_attack"
+  }
+}
+
+resource "cloudflare_page_rule" "spring_maps_old" {
+  zone_id  = local.cloudflare_zone_id
+  priority = cloudflare_page_rule.seichi_maps_old.priority
+  target   = "*-map-spring.${local.root_domain}/*"
+
+  actions {
+    security_level = "under_attack"
+  }
+}
+
+resource "cloudflare_page_rule" "seichi_ranking" {
+  zone_id  = local.cloudflare_zone_id
+  priority = cloudflare_page_rule.spring_maps_old.priority
+  target   = "ranking-gigantic.${local.root_domain}/*"
+
+  actions {
+    security_level = "under_attack"
+  }
+}
+
+resource "cloudflare_page_rule" "seichi_maps" {
+  zone_id  = local.cloudflare_zone_id
+  priority = cloudflare_page_rule.seichi_ranking.priority
+  target   = "*.map.gigantic.${local.root_domain}/*"
 
   actions {
     security_level = "under_attack"
@@ -12,20 +42,8 @@ resource "cloudflare_page_rule" "spring_maps" {
   depends_on = [cloudflare_page_rule.seichi_maps]
 
   zone_id  = local.cloudflare_zone_id
-  priority = 2
-  target   = "*-map-spring.${local.root_domain}/*"
-
-  actions {
-    security_level = "under_attack"
-  }
-}
-
-resource "cloudflare_page_rule" "seichi_ranking" {
-  depends_on = [cloudflare_page_rule.spring_maps]
-
-  zone_id  = local.cloudflare_zone_id
-  priority = 3
-  target   = "ranking-gigantic.${local.root_domain}/*"
+  priority = cloudflare_page_rule.seichi_maps.priority
+  target   = "*.map.spring.${local.root_domain}/*"
 
   actions {
     security_level = "under_attack"
