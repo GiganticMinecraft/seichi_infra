cbb9b363e7d095bb4edd789d5df7d3d8ae557578
inductor <kela@inductor.me>
2022-06-05 23:17:18 +0900

    add local vars

--- terraform/onp_cluster/main.tf
+++ terraform/onp_cluster/main.tf
@@ -22,3 +22,13 @@ variable "cloudflared_tunnel_credential" {
   type          = string
   sensitive     = true
 }
+
+variable "minio_root_user" {
+  type          = string
+  sensitive     = true
+}
+
+variable "minio_root_password" {
+  type          = string
+  sensitive     = true
+}
