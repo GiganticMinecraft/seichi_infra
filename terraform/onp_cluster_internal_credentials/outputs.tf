2dff1c3d6642fcc8af2d73753de71bb199f6304d
Kory | Ryosuke Kondo <6561358+kory33@users.noreply.github.com>
2022-08-13 17:47:04 +0900

    fix: do not explicitly mark output as sensitive

--- terraform/onp_cluster_internal_credentials/outputs.tf
+++ terraform/onp_cluster_internal_credentials/outputs.tf
@@ -1,9 +1,7 @@
 output "influxdb_admin_password" {
   value     = random_password.influxdb_admin_password
-  sensitive = true
 }
 
 output "influxdb_admin_token" {
   value     = random_password.influxdb_admin_token
-  sensitive = true
 }
