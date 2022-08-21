f579915273f045831a1cd026c858daeb53e1b25a
Ryosuke <korygm33@gmail.com>
2022-08-21 23:42:19 +0900

    feat: expose game-data server on `seichi-game-data.public-gigantic-api.seichi.click`

--- terraform/cloudflare_network_services.tf
+++ terraform/cloudflare_network_services.tf
@@ -8,6 +8,7 @@ resource "cloudflare_certificate_pack" "advanced_cert_for_services" {
     # 整地鯖が提供している経路
     "*.gigantic.${local.root_domain}",
     "*.map.gigantic.${local.root_domain}",
+    "*.public-gigantic-api.${local.root_domain}",
 
     # 整地鯖（春）が提供している経路
     "*.spring.${local.root_domain}",
