bc6b842540f152af15b13d4061b6e0714f7499fa
inductor <kela@inductor.me>
2022-08-23 23:37:40 +0900

    remove generated password

--- terraform/onp_cluster_internal_credentials/influxdb_auth.tf
+++
@@ -1,9 +0,0 @@
-resource "random_password" "influxdb_admin_password" {
-  length  = 48
-  special = true
-}
-
-resource "random_password" "influxdb_admin_token" {
-  length  = 48
-  special = true
-}
--- terraform/onp_cluster_internal_credentials/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "influxdb_admin_password" {
-  value     = random_password.influxdb_admin_password.result
-  sensitive = true
-}
-
-output "influxdb_admin_token" {
-  value     = random_password.influxdb_admin_token.result
-  sensitive = true
-}
