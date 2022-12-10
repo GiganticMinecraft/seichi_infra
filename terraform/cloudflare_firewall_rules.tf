8c5dffd916b86ef667f9596313590e406ed6c943
inductor <kela@inductor.me>
2022-12-10 23:53:58 +0900

    add phpMyAdmin

--- terraform/cloudflare_firewall_rules.tf
+++ terraform/cloudflare_firewall_rules.tf
@@ -11,3 +11,17 @@ resource "cloudflare_firewall_rule" "bypass_waf_on_minio_api_requests" {
   action      = "bypass"
   products    = toset(["waf"])
 }
+
+resource "cloudflare_filter" "phpmyadmin_api_requests" {
+  zone_id     = local.cloudflare_zone_id
+  description = "phpMyAdmin API requests"
+  expression  = "http.host eq \"phpmyadmin.onp-k8s.admin.seichi.click\""
+}
+
+resource "cloudflare_firewall_rule" "bypass_waf_on_phpmyadmin_requests" {
+  zone_id     = local.cloudflare_zone_id
+  description = "Let phpMyAdmin requests bypass WAF rules"
+  filter_id   = cloudflare_filter.phpmyadmin_api_requests.id
+  action      = "bypass"
+  products    = toset(["waf"])
+}
