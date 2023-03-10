d0e72861de91e433d55bcc9295ca27a5f27c8db0
Lucky3028 <Lucky3028@users.noreply.github.com>
2023-03-10 15:17:12 +0900

    feat: add cloudflare pages project for seichi-portal

---
+++ terraform/cloudflare_page_projects.tf
@@ -0,0 +1,27 @@
+#region seichi-portal
+
+resource "cloudflare_pages_project" "seichi_portal" {
+  account_id        = ""
+  name              = "seichi-portal"
+  production_branch = "main"
+  build_config {
+    build_command   = "yarn build"
+    destination_dir = "out"
+    root_dir        = "/"
+  }
+  source {
+    type = "github"
+    config {
+      owner     = local.github_org_name
+      repo_name = "seichi-portal-frontend"
+    }
+  }
+}
+
+resource "cloudflare_pages_domain" "seichi_portal_domain" {
+  account_id   = ""
+  project_name = cloudflare_pages_project.seichi_portal.name
+  domain       = "portal.${local.root_domain}"
+}
+
+#endregion
