5aeebd60845c987937acf89ea5a82a4ab79fd404
Lucky3028 <Lucky3028@users.noreply.github.com>
2023-03-12 12:51:27 +0900

    fix: use account_id

--- terraform/cloudflare_page_projects.tf
+++ terraform/cloudflare_page_projects.tf
@@ -1,7 +1,7 @@
 #region seichi-portal
 
 resource "cloudflare_pages_project" "seichi_portal" {
-  account_id        = ""
+  account_id        = local.cloudflare_account_id
   name              = "seichi-portal"
   production_branch = "main"
   build_config {
@@ -20,7 +20,7 @@ resource "cloudflare_pages_project" "seichi_portal" {
 }
 
 resource "cloudflare_pages_domain" "seichi_portal_domain" {
-  account_id   = ""
+  account_id   = local.cloudflare_account_id
   project_name = cloudflare_pages_project.seichi_portal.name
   domain       = "portal.${local.root_domain}"
 }
