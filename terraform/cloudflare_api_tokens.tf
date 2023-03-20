d807150da38de580901e7c8a8f6503719712642e
Lucky3028 <Lucky3028@users.noreply.github.com>
2023-03-20 17:53:24 +0900

    feat: add an api token for Cloudflare Pages

---
+++ terraform/cloudflare_api_tokens.tf
@@ -0,0 +1,14 @@
+data "cloudflare_api_token_permission_groups" "all" {}
+
+resource "cloudflare_api_token" "pages_api_token" {
+  name = "pages-api-token"
+
+  policy {
+    permission_groups = [
+      data.cloudflare_api_token_permission_groups.all.permissions["Cloudflare Pages Edit"],
+    ]
+    resources = {
+      "com.cloudflare.api.account.*" = "*"
+    }
+  }
+}
