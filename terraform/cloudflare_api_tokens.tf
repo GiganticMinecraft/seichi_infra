3d7ac82c3b9ffe6db6f889958c3d24ce69b67a1b
Lucky3028 <Lucky3028@users.noreply.github.com>
2023-03-20 18:10:10 +0900

    fix: fix permission key name

--- terraform/cloudflare_api_tokens.tf
+++ terraform/cloudflare_api_tokens.tf
@@ -5,7 +5,7 @@ resource "cloudflare_api_token" "pages_api_token" {
 
   policy {
     permission_groups = [
-      data.cloudflare_api_token_permission_groups.all.permissions["Access: Audit Logs Read"],
+      data.cloudflare_api_token_permission_groups.all.permissions["Cloudflare Pages Edit"],
     ]
     resources = {
       "com.cloudflare.api.account.*" = "*"
