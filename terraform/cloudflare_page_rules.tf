a4ea3831b3a24235bfc81a8c790b7b020e224bc0
Ryosuke <korygm33@gmail.com>
2023-06-14 07:21:12 +0100

    feat: sentry API の security を off にする

--- terraform/cloudflare_page_rules.tf
+++ terraform/cloudflare_page_rules.tf
@@ -28,7 +28,7 @@ resource "cloudflare_page_rule" "spring_maps" {
   }
 }
 
-resource "cloudflare_page_rule" "foobar" {
+resource "cloudflare_page_rule" "cache_public_game_data_api" {
   zone_id  = local.cloudflare_zone_id
   priority = cloudflare_page_rule.spring_maps.priority + 1
   target   = "seichi-game-data.public-gigantic-api.${local.root_domain}/*"
@@ -38,3 +38,14 @@ resource "cloudflare_page_rule" "foobar" {
   }
 }
 
+resource "cloudflare_page_rule" "disable_security_on_sentry_api" {
+  zone_id  = local.cloudflare_zone_id
+  priority = cloudflare_page_rule.cache_public_game_data_api.priority + 1
+  target   = "sentry.onp.admin.${local.root_domain}/api/*"
+
+  actions {
+    # Security を disable しない場合、Sentry の performance monitoring 用の API コールが
+    # Cloudflare の challenge に引っかかってしまうため disable している
+    disable_security = true
+  }
+}
