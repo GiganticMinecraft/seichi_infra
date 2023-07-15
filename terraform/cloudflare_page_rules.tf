69e3110ec56c275fb455efc605fcb2ae1ca9bb0a
rito528 <39003544+rito528@users.noreply.github.com>
2023-07-15 22:11:16 +0900

    chore: seichi_rankingを消す

--- terraform/cloudflare_page_rules.tf
+++ terraform/cloudflare_page_rules.tf
@@ -15,19 +15,9 @@ resource "cloudflare_page_rule" "global" {
   }
 }
 
-resource "cloudflare_page_rule" "seichi_ranking" {
-  zone_id  = local.cloudflare_zone_id
-  priority = cloudflare_page_rule.global.priority + 1
-  target   = "ranking-gigantic.${local.root_domain}/*"
-
-  actions {
-    security_level = "under_attack"
-  }
-}
-
 resource "cloudflare_page_rule" "seichi_maps" {
   zone_id  = local.cloudflare_zone_id
-  priority = cloudflare_page_rule.seichi_ranking.priority + 1
+  priority = cloudflare_page_rule.global.priority + 1
   target   = "*.map.gigantic.${local.root_domain}/*"
 
   actions {
