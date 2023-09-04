20482addf12490012cef877cfbcc87657bb4d4a6
outductor <inductor.kela+seichi@gmail.com>
2023-09-05 00:01:03 +0900

    revert

--- seichi-onp-k8s/cluster-boot-up/scripts/nodes/k8s-node-setup.sh
+++ seichi-onp-k8s/cluster-boot-up/scripts/nodes/k8s-node-setup.sh
@@ -92,10 +92,10 @@ sudo apt-get update && sudo apt-get install -y containerd.io
 sudo mkdir -p /etc/containerd
 sudo containerd config default | sudo tee /etc/containerd/config.toml > /dev/null
 
-if grep -q "systemd_cgroup = false" "/etc/containerd/config.toml"; then
+if grep -q "SystemdCgroup = true" "/etc/containerd/config.toml"; then
 echo "Config found, skip rewriting..."
 else
-sed -i -e "s/systemd_cgroup \= false/systemd_cgroup \= true/g" /etc/containerd/config.toml
+sed -i -e "s/SystemdCgroup \= false/SystemdCgroup \= true/g" /etc/containerd/config.toml
 fi
 
 sudo systemctl restart containerd
