3785c1f74e2f506ad5dcb5890e892f52e98124df
outductor <inductor.kela+seichi@gmail.com>
2024-09-08 20:22:24 +0900

    fix script

--- seichi-onp-k8s/cluster-operation/scripts/nodes/k8s-node-join.sh
+++ seichi-onp-k8s/cluster-operation/scripts/nodes/k8s-node-join.sh
@@ -25,7 +25,7 @@ sudo sysctl --system
 
 # Add Docker's official GPG key:
 sudo apt-get update
-sudo apt-get install apt-transport-https ca-certificates curl gnupg2
+sudo apt-get install -y apt-transport-https ca-certificates curl gnupg2
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
