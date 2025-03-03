e7b1133275f7b7ef8c18693c11d762549455cab7
rito528 <39003544+rito528@users.noreply.github.com>
2025-03-03 15:03:40 +0900

    feat: s1 用の定義を追加

--- terraform/onp_cluster_minecraft_secrets.tf
+++ terraform/onp_cluster_minecraft_secrets.tf
@@ -104,6 +104,31 @@ resource "kubernetes_secret" "onp_minecraft_prod_mariadb_root_password" {
   type = "Opaque"
 }
 
+variable "namespaces-to-deploy-pbs-credentials" {
+  type    = list(string)
+  default = ["seichi-minecraft", "minio"]
+}
+
+resource "kubernetes_secret" "onp_minecraft_pbs_credentials" {
+  for_each = toset(var.namespaces-to-deploy-pbs-credentials)
+
+  metadata {
+    name      = "pbs-credentials"
+    namespace = each.value
+  }
+
+  data = {
+    user        = var.proxmox_backup_client__user
+    host        = var.proxmox_backup_client__host
+    datastore   = var.proxmox_backup_client__datastore
+    password    = var.proxmox_backup_client__password
+    fingerprint = var.proxmox_backup_client__fingerprint
+  }
+
+  type = "Opaque"
+}
+
+
 resource "random_password" "minecraft__debug_mariadb_root_password" {
   length           = 16
   special          = true
@@ -160,13 +185,13 @@ resource "helm_release" "onp_minecraft_debug_minio_secrets" {
 
 }
 
-variable "namespaces-to-deploy-pbs-credentials" {
+variable "namespaces-to-deploy-debug-pbs-credentials" {
   type    = list(string)
   default = ["seichi-debug-minecraft", "minio"]
 }
 
 resource "kubernetes_secret" "onp_minecraft_debug_pbs_credentials" {
-  for_each = toset(var.namespaces-to-deploy-pbs-credentials)
+  for_each = toset(var.namespaces-to-deploy-debug-pbs-credentials)
 
   metadata {
     name      = "pbs-credentials"
