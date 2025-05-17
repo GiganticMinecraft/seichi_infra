0b21d6b40eab42428817723497600d34b5084ca3
Sho Sakuma <me@m1sk9.dev>
2025-05-17 18:33:55 +0000

    feat: Add seichi-playguide

--- terraform/cloudflare_page_projects.tf
+++ terraform/cloudflare_page_projects.tf
@@ -13,3 +13,19 @@ resource "cloudflare_pages_domain" "seichi_portal_domain" {
 }
 
 #endregion
+
+#region seichi-playguide
+
+resource "cloudflare_pages_project" "seichi_playguide" {
+  account_id        = local.cloudflare_account_id
+  name              = "seichi-playguide"
+  production_branch = "main"
+}
+
+resource "cloudflare_pages_domain" "seichi_playguide_domain" {
+  account_id   = local.cloudflare_account_id
+  project_name = cloudflare_pages_project.seichi_playguide.name
+  domain       = "seichi-playguide.${local.root_domain}"
+}
+
+#endregion
